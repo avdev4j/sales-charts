190c283f35b53093a233c4b9f1d2daebb1dc1f23
Anthony Viard <anth.viard@gmail.com>
2021-10-29 16:56:37 +0200

    Update the endpoint

--- ui/widgets/charts/line-chart/src/integration/Integration.js
+++ ui/widgets/charts/line-chart/src/integration/Integration.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const endpoint = `${process.env.REACT_APP_PUBLIC_API_URL}/stats/commits`
+const endpoint = `${process.env.REACT_APP_PUBLIC_API_URL}/stats/sales`
 
 export const getData = async ()=>
   await axios.get(endpoint)
